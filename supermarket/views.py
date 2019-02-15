274b2a29146cddb8f55326d6c68c1d3e868a8c8e
Ali <mrpalitaev@gmail.com>
2019-02-15 21:52:17 +0600

    Refactored main page, added infinite scroll

--- supermarket/views.py
+++ supermarket/views.py
@@ -1,13 +1,26 @@
 from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+from applications.categories.models import Category
+from applications.products.models import Product
 from .forms import ContactsForm, LoginForm, RegistrationForm
 
 User = get_user_model()
 
 
 def main_page(request):
+    categories = Category.objects.all()
+    products_list = Product.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(products_list, 9)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, 'index.html', locals())
 
 
